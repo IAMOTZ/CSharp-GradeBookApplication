459be90d430688005ea81031485d9b720f759262
Tunmise <ogunniyitunmise@gmail.com>
2019-03-20 22:26:03 +0100

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,33 @@ namespace GradeBook.GradeBooks
         {
             base.Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (base.Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+
+            double twentyPercentOfStudents = (base.Students.Count/5);
+
+            List<double> averageGrades = new List<double>();
+            foreach(Student student in base.Students)
+            {
+                if(!averageGrades.Exists((grade) => grade == student.AverageGrade))
+                    averageGrades.Add(student.AverageGrade);
+            }
+            averageGrades.Sort();
+            averageGrades.Reverse();
+            if (averageGrades.IndexOf(averageGrade) == -1)
+                return 'F';
+            if (averageGrades.IndexOf(averageGrade) < (twentyPercentOfStudents))
+                return 'A';
+            else if (averageGrades.IndexOf(averageGrade) < (2*twentyPercentOfStudents))
+                return 'B';
+            else if (averageGrades.IndexOf(averageGrade) < (3*twentyPercentOfStudents))
+                return 'C';
+            else if (averageGrades.IndexOf(averageGrade) < (4*twentyPercentOfStudents))
+                return 'D';
+            return 'F';
+        }
     }
 }
